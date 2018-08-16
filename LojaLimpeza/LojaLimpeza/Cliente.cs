b5985d9af151a22a790f1adb2b515b075a33edf1
Marcos Pereira <marcosppereira16@hotmail.com>
2018-08-16 15:14:28 -0300

    StatusCliente feito

---
+++ LojaLimpeza/LojaLimpeza/Cliente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaLimpeza
+{
+    public class Cliente
+    {
+        private int limiteDeCredito;
+
+        public int IdCliente { get; set; }
+        public string NomeCliente { get; set; }
+        public string CPF { get; set; }
+        public string Filiacao { get; set; }
+        public Contato Contato { get; set; }
+        public Endereco Endereco { get; set; }
+        public StatusDoCliente StatusDoCliente { get; set; }
+        public int LimiteDeCredito { get { return this.limiteDeCredito; } }
+        public IList<Pedido> ListaDePedido = new List<Pedido>();
+
+        public Cliente(int id, string nomeDoCliente, string cpf, string filiacao, Contato contato, Endereco endereco, int limiteDeCredito)
+        {
+            this.IdCliente = id;
+            this.NomeCliente = nomeDoCliente;
+            this.CPF = cpf;
+            this.Filiacao = filiacao;
+            this.Contato = contato;
+            this.Endereco = endereco;
+            this.limiteDeCredito = limiteDeCredito;
+        }
+    }
+}
---
+++ LojaLimpeza/LojaLimpeza/StatusCliente.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaLimpeza
+{
+    public class StatusDoCliente
+    {
+        public int IdStatus { get; set; }
+        public IList<Cliente> ListaDeCliente = new List<Cliente>();
+        public string NomeStatus { get; set; }
+
+        public void VerificarStatusCliente(Cliente cliente)
+        {
+            foreach (var listaDeCliente in ListaDeCliente)
+            {
+                if (cliente.LimiteDeCredito < 1000)
+                {
+                    this.IdStatus = 1;
+                }
+                else if (cliente.LimiteDeCredito <= 3000)
+                {
+                    this.IdStatus = 2;
+                }
+                else if (cliente.LimiteDeCredito > 3000)
+                {
+                    this.IdStatus = 3;
+                }
+                else
+                {
+                    throw new Exception("Status do cliente inváido");
+                }
+            }
+        }
+    }
+}
