ec27fb53f87a269f428f2b457bd9946ea5044e89
MarcosVPP <marcosppereira16@hotmail.com>
2018-08-16 23:39:16 -0300

    Classes Restantes e Correções em Variáveis

---
+++ LojaLimpeza/LojaLimpeza/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaLimpeza
+{
+    public class Categoria
+    {
+        public int CodigoCategoria { get; set; }
+        public string NomeCategoria { get; set; }
+        public IList<Produto> ListaProduto = new List<Produto>();
+
+        public Categoria(int id, string nome)
+        {
+            this.CodigoCategoria = id;
+            this.NomeCategoria = nome;
+        }
+    }
+}
---
+++ LojaLimpeza/LojaLimpeza/Fornecedor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaLimpeza
+{
+    public class Fornecedor
+    {
+        public int CodigoFornecedor { get; set; }
+        public string CNPJ { get; set; }
+        public string NomeFantasia { get; set; }
+        public string RazaoSocial { get; set; }
+
+        public Fornecedor(int id, string cnpj, string nomeFantasia, string razaoSocial)
+        {
+            this.CodigoFornecedor = id;
+            this.CNPJ = cnpj;
+            this.NomeFantasia = nomeFantasia;
+            this.RazaoSocial = razaoSocial;
+        }
+    }
+}
