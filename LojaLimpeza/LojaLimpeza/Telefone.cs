f96295d00698a4e90e558872289d5ce40b01e078
JuanDiasV <33902980+JuanDiasV@users.noreply.github.com>
2018-08-17 09:21:50 -0300

    Quase

---
+++ LojaLimpeza/LojaLimpeza/Telefone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaLimpeza
+{
+   public class Telefone
+    {
+        public int IdTelefone { get; set; }
+        public int Numero1 { get; set; }
+        public int Numero2 { get; set; }
+        public int Numero3 { get; set; }
+    }
+}
