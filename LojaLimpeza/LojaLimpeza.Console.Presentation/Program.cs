1434e8388f2edbb86f9827d80429c2aa85346063
JuanDiasV <33902980+JuanDiasV@users.noreply.github.com>
2018-08-17 15:06:28 -0300

    d

--- LojaLimpeza/LojaLimpeza.Console.Presentation/Program.cs
+++ LojaLimpeza/LojaLimpeza.Console.Presentation/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LojaLimpeza.Service;
 
 namespace LojaLimpeza.Console.Presentation
 {
@@ -33,16 +34,19 @@ namespace LojaLimpeza.Console.Presentation
 
         private static void Cadastro()
         {
-            System.Console.WriteLine("Informe os dados para cadastro");
-            System.Console.Write("Código: ");
-            var codigo = int.Parse(System.Console.ReadLine());
+            /*    System.Console.WriteLine("Informe os dados para cadastro");
+                System.Console.Write("Código: ");
+                var codigo = int.Parse(System.Console.ReadLine());
 
-            System.Console.Write("Nome: ");
-            var nome = System.Console.ReadLine();
+                System.Console.Write("Nome: ");
+                var nome = System.Console.ReadLine();
 
-            Domain.Categoria categoria = new Domain.Categoria(codigo, nome);
+                Domain.Categoria categoria = new Domain.Categoria(codigo, nome);
 
-            categoriaService.Salvar(categoria);
+                categoriaService.Salvar(categoria);
+           */
+
+
         }
     }
 }
